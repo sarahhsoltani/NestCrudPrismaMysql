f144a1da2a99902aead39822735bf4a596aca576
sarahhsoltani <sarasoltani.simplon.enda@gmail.com>
2023-10-10 15:54:42 +0000

    fix

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
+
+const port=process.env.PORT
+  await app.listen(port);
+}
+bootstrap();
